bdb1ed52857228ce8285bdbf0a160399ada1ea4b
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2021-11-11 16:42:31 +0530

    error

--- orders.php
+++ orders.php
@@ -114,25 +114,3 @@ $run_order_count = mysqli_query($con,$get_order_count);
     <h6 class="mt-1" style="margin-bottom:3px;margin-top:0px!important;">₹ <?php echo ($total_earnings+$total_bonus)-$total_salary; ?></h6>
   </div>
 </div>
-<div class="row">
-  <?php for ($x = 0; $x <= 10; $x++) {?>
-  <div class="col-lg-12 col-md-6">
-    <div class="card card-chart p-2">
-      <div class="row">
-        <div class="col-6">
-          <h6 class="badge badge-warning p-1">Earning ₹10</h6>
-          <h6 class="card-title">Name : Tejas Shirsat</h6>
-          <h6 class="card-title">Mobile : 9867765397</h6>
-        </div>
-        <div class="col-6 text-right">
-          <button class="btn btn-primary mb-1 mt-0 py-1">TAKE ₹100</button>
-          <button type="button" class="btn btn-info py-1 mt-0">Click Here</button>
-        </div>
-      </div>
-      <h6 class="card-title">Address : Demo address demo address demo address.</h6>
-      <button type="button" class="btn btn-info text-left">View</button>
-      <button type="button" class="btn btn-success pull-right">Delivered</button>
-    </div>
-  </div>
-<?php }?>
-</div>
